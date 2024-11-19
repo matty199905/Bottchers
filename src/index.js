f236888cbf0f0d0f0c56aaece57b719d4b8d09a7
matty199905 <matty199905@gmail.com>
2024-11-19 15:02:17 -0300

    last

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './Styles/GlobalStyles';
-import {Provider} from 'react-redux'
-import {PersistGate} from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './Redux/store';
 
 
@@ -15,10 +15,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-    <GlobalStyles />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </PersistGate>
   </Provider>
 
