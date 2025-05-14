5cbd3f041be7298c456696cd1f88d04d5aa47ca0
matty199905 <matty199905@gmail.com>
2025-05-14 11:42:23 -0300

    tsx-migration

--- src/Utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const SHIPPINGCOST = 2500
-export const APIURL = 'https://nucbaz-api.vercel.app'
--- src/Utils/formatPrice.js
+++
@@ -1,6 +0,0 @@
-export const formatPrice = (price) => {
-    return price.toLocaleString('es-AR', {
-        style: 'currency',
-        currency: 'ARS'
-    })
-}
